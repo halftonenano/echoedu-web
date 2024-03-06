0d5ced3d50bca0d0ca586dd48324548f7bf67283
halftone <86031051+halftonenano@users.noreply.github.com>
2024-03-05 18:16:41 -0800

    updated signup page and flow

--- src/routes/api/tutoring/signup/+server.ts
+++ src/routes/api/tutoring/signup/+server.ts
@@ -1,31 +1,42 @@
-import { pb } from "$lib/pocketbase.js";
-import { initAdminPb } from "$lib/server/pb-admin.js";
-import type {
-  TutorsRecord
-} from "$lib/types/db.js";
+import { pb } from '$lib/pocketbase.js';
+import { initAdminPb } from '$lib/server/pb-admin.js';
+import type { TutorsRecord } from '$lib/types/db.js';
 
-import { json } from "@sveltejs/kit";
+import { json } from '@sveltejs/kit';
 
 export async function POST({ request }) {
-  pb.authStore.loadFromCookie(request.headers.get("Cookie") || "");
-  if (!pb.authStore.model?.id)
-    return json({ error: "you are not signed in" }, { status: 403 });
+	pb.authStore.loadFromCookie(request.headers.get('Cookie') || '');
+	if (!pb.authStore.model?.id) return json({ error: 'you are not signed in' }, { status: 403 });
 
-  const admin = await initAdminPb();
+	try {
+		await pb.collection('tutors').getFirstListItem(`user="${pb.authStore.model.id}"`);
+		return json({ error: 'You are already a tutor' });
+	} catch (e) {
+		console.error(e);
+	}
 
-  try {
-    const record = await admin
-      .collection("autoNhsIds")
-      .getFirstListItem(`email="${pb.authStore.model.email}"`);
+	const admin = await initAdminPb();
 
-		await admin.collection('tutors').create({ user: pb.authStore.model.id, name: pb.authStore.model.name, isNHS: true } satisfies TutorsRecord);
+	try {
+		const record = await admin
+			.collection('autoNhsIds')
+			.getFirstListItem(`email="${pb.authStore.model.email}"`);
 
-    return json({ success: "You have been auto approved!" });
-  } catch {
-    await admin
-      .collection("applications")
-      .create({ user: pb.authStore.model.id });
+		await admin.collection('tutors').create({
+			user: pb.authStore.model.id,
+			name: pb.authStore.model.name,
+			isNHS: true
+		} satisfies TutorsRecord);
 
-    return json({ success: "Application has been submitted" });
-  }
+		return json({ success: 'You have been auto approved!' });
+	} catch {
+		try {
+			await admin.collection('applications').getFirstListItem(`user="${pb.authStore.model.id}"`);
+			return json({ error: 'You have already submitted an application' });
+		} catch {}
+
+		await admin.collection('applications').create({ user: pb.authStore.model.id });
+
+		return json({ success: 'Application has been submitted' });
+	}
 }
--- src/routes/tutoring/signup/+page.svelte
+++ src/routes/tutoring/signup/+page.svelte
@@ -1,58 +1,85 @@
 <script>
+	import { goto } from '$app/navigation';
 	import PageFormat from '$lib/components/PageFormat.svelte';
 	import { Button, buttonVariants } from '$lib/components/ui/button';
+	import { user } from '$lib/pocketbase';
+	import { cn, sleep } from '$lib/utils';
 	import { confetti } from '@neoconfetti/svelte';
 	import { Loader2 } from 'lucide-svelte';
 	import toast from 'svelte-french-toast';
-  import { user } from '$lib/pocketbase'
-	import { cn, sleep } from '$lib/utils';
-	import { goto } from '$app/navigation';
 
-  let processing = false;
-  let success = '';
+	let processing = false;
+	let success = '';
 </script>
 
-<PageFormat title='EchoEDU Tutoring' description="Sign up as a tutor for EchoEDU">
-  {#if success}
-    <div class="mx-auto">
-      <div use:confetti />
-    </div>
-  {/if}
+<PageFormat title="EchoEDU Tutoring" description="Sign up as a tutor for EchoEDU">
+	{#if success === 'You have been auto approved!'}
+		<div class="mx-auto">
+			<div use:confetti />
+		</div>
+	{/if}
 
-  {#if $user}
-    <Button
-      class="flex w-full items-center justify-center gap-2"
-      on:click={async () => {
-        processing = true;
+	{#if $user}
+		<p class="mb-5">If you are in NHS you will be auto approved!</p>
 
-        const response = await (
-          await fetch(`/api/tutoring/signup`, { method: 'POST' })
-        ).json();
+		<Button
+			class="flex w-full items-center justify-center gap-2"
+			variant="outline"
+			on:click={async () => {
+				processing = true;
 
-        success = response.success
+				const response = await (await fetch(`/api/tutoring/signup`, { method: 'POST' })).json();
 
-        processing = false;
+				if (response.error) {
+					toast.error(response.error);
+				}
+				success = response.success || '';
 
-        if (success === 'You have been auto approved!') {
-          await sleep(3000);
-          await goto('/tutoring');
-        }
-      }}
-      disabled={processing || success !== ''}
-    >
-    
-      {#if processing}
-        Applying...<Loader2 class="h-5 animate-spin" />
-      {:else if success !== ''}
-        {success} 🎊
-      {:else}
-        Apply
-      {/if}
-    </Button>
-  {:else}
-    <a href="/signin?after=/tutoring/signup" class={cn("w-full flex h-14 items-center gap-3 bg-zinc-100 p-3 px-6", buttonVariants({ variant: "outline" }))}>
-      <img src={'/assets/google.png'} alt="google logo" class="h-5" />
-      Continue to sign in
-    </a>
-  {/if}
+				processing = false;
+
+				if (success === 'You have been auto approved!') {
+					await sleep(3000);
+					await goto('/tutoring');
+				}
+			}}
+			disabled={processing || success !== ''}
+		>
+			{#if processing}
+				Applying...<Loader2 class="h-5 animate-spin" />
+			{:else if success !== ''}
+				{success} 🎊
+			{:else}
+				Apply
+			{/if}
+		</Button>
+
+		<p class="my-5">
+			Please fill out this form after applying with the button above. NHS members will not have to
+			do this.
+		</p>
+
+		<iframe
+			class="mx-auto"
+			title="google form"
+			src="https://docs.google.com/forms/d/e/1FAIpQLSemn4XOS8igDLlpWvQa4v9Nw-SisQ5-ujXophDct01WflGW9A/viewform?embedded=true"
+			width="640"
+			height="1200"
+			frameborder="0"
+			marginheight="0"
+			marginwidth="0">Loading…</iframe
+		>
+	{:else}
+		<a
+			href="/signin?after=/tutoring/signup"
+			class={cn(
+				'flex h-14 w-full items-center gap-3 bg-zinc-100 p-3 px-6',
+				buttonVariants({ variant: 'outline' })
+			)}
+		>
+			<img src={'/assets/google.png'} alt="google logo" class="h-5" />
+			Sign in to continue
+		</a>
+
+		<div class="md:h-52"></div>
+	{/if}
 </PageFormat>
